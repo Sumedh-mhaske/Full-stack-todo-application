d3ab0a9a0cb8e461f31369cf4900680a1d437ad4
Sumedh <sumedhmhaske310503@gmail.com>
2025-05-16 16:08:38 +0530

    Adding input todos in the backend

--- Frontend/src/Components/CreateTodo.jsx
+++ Frontend/src/Components/CreateTodo.jsx
@@ -1,28 +1,46 @@
+import { useState } from "react";
+
 export function CreateTodo() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const inputStyle = { padding: 10, margin: 10 };
+
   return (
     <div>
       <input
-        style={{
-          padding: 10,
-          margin: 10,
-        }}
+        style={inputStyle}
         type="text"
         placeholder="Title"
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
       />
       <br />
       <input
-        style={{
-          padding: 10,
-          margin: 10,
-        }}
+        style={inputStyle}
         type="text"
         placeholder="Description"
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
       />
       <br />
       <button
-        style={{
-          padding: 10,
-          margin: 10,
+        style={inputStyle}
+        onClick={() => {
+          fetch("http://localhost:3000/todo", {
+            method: "POST",
+            body: JSON.stringify({
+              title: title,
+              description: description,
+            }),
+            headers: {
+              "Content-type": "applicaton/json",
+            },
+          }).then(async function (res) {
+            const json = await res.json();
+            alert("Todo added");
+          });
         }}
       >
         Add a todo
